d59b0ebe057000bc8eccc22da58d899f83f3d1ca
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-11 21:59:10 -0500

    Setup pages files

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class SingleRoom extends
+Component {
+    render() {
+        return (
+            <div>
+                Hello SingleRoom
+            </div>
+        )
+    }
+}
