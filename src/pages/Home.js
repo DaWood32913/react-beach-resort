66cf1d0efdc97995cd0519023f8cb781acd1e454
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-14 17:40:51 -0500

    setup hero component for home, room, and error pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import Hero from "../components/Hero"
 
 export default function Home() {
     return (
-        <div>
-            Hello Home
-        </div>
+        <Hero hero ="defaultHero" />
+        
     )
     
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import Hero  from '../components/Hero'
 
 const Rooms = () => {
-    return <div>
-             Hello Rooms
-           </div>;
+    return <Hero hero="roomsHero" />
 };
 
 export default Rooms;
