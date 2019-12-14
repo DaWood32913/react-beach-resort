d90d197d755b25f25b7a42a4437273adf186b800
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-14 18:20:43 -0500

    started services component

---
+++ src/components/Services.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Title from './Title'
+
+export class Services extends Component {
+
+    render() {
+        return (
+            <div>
+                <Title title="service" />
+            </div>
+        )
+    }
+}
+
+export default Services
