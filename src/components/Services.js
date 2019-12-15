4c30a2a93f583a6d6ff2e4841a5b16b4edd565c8
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-15 13:21:27 -0500

    setup services with font awesome icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,15 +1,54 @@
-import React, { Component } from 'react'
-import Title from './Title'
+import React, { Component } from "react";
+import Title from "./Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
 export class Services extends Component {
-
-    render() {
-        return (
-            <div>
-                <Title title="service" />
-            </div>
-        )
-    }
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      },
+      {
+        icon: <FaHiking />,
+        title: "endless hiking",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "free shuttle",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      },
+      {
+        icon: <FaBeer />,
+        title: "ice cold beer",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      }
+    ]
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
 }
 
-export default Services
+export default Services;
