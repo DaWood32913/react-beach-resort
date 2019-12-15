3ecff89a4606e5947f33e1cc85037fd65bf49581
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-15 16:47:17 -0500

    i unbroke this somehow

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { RoomContext } from "../context";
+
+export class FeaturedRooms extends Component {
+    static contextType = RoomContext
+    render() {
+        
+        return (
+            <div>
+              
+            </div>
+        )
+    }
+}
+
+export default FeaturedRooms
