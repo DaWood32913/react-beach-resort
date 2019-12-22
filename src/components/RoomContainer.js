3e4e50e499433be41f93f38cac466c00b33e577c
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-21 23:05:04 -0500

    set up room container, filter, and list files for rooms page

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList'
+
+export default function RoomsContainer() {
+    return (
+        <div>
+            hello RoomsContainer
+            <RoomFilter />
+            <RoomList />
+        </div>
+    )
+}
