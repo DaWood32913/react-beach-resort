61c4780c9607244ba0125bca0dd68a8011d2c948
DaWood32913 <woodleyjohnson@yahoo.com>
2020-01-23 12:23:12 -0500

    added API keys to get data from contentful and not locally. deleted get for local data. created env. variable to hide keys

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import items from "./data";
+// import items from "./data";
+import Client from "./Contentful";
 
 const RoomContext = React.createContext();
 // <RoomContext.Provider value={'hello'}
@@ -22,24 +23,33 @@ class RoomProvider extends Component {
   };
 
   //getData
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortRooms",
+        // order: "sys.createdAt"
+      });
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   componentDidMount() {
-    // this.getData
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      price: maxPrice,
-      maxPrice,
-      maxSize
-    });
+    this.getData();
   }
-
   formatData(items) {
     let tempItems = items.map(item => {
       let id = item.sys.id;
@@ -86,7 +96,7 @@ class RoomProvider extends Component {
     let tempRooms = [...rooms];
 
     //transform value
-    capacity = parseInt(capacity)
+    capacity = parseInt(capacity);
 
     //filter by type
     if (type !== "all") {
@@ -94,25 +104,27 @@ class RoomProvider extends Component {
     }
 
     //filter by capacity
-    if(capacity !==1) {
-      tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
 
     //filter by price
     tempRooms = tempRooms.filter(room => room.price <= price);
 
     //filter by size
-    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
 
     //filter by breakfast
-    if(breakfast){
-      tempRooms = tempRooms.filter(room => room.breakfast === true)
-    };
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
 
     //filter by pets
-    if(pets){
-      tempRooms = tempRooms.filter(room => room.pets === true)
-    };
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
 
     //change state
     this.setState({
